2d32d18d8b175875183c2500548a0400f0fd5aab
Alan Dean <adean3032@gmail.com>
2023-01-16 05:44:19 -0500

    final

--- db/seed.js
+++ db/seed.js
@@ -6,7 +6,10 @@ const {
   updateUser,
   getAllPosts,
   createPost, 
-  updatePost
+  updatePost, 
+  createTags, 
+  addTagsToPost, 
+  getPostsByTagName
 } = require('./index');
 
 async function dropTables() {
@@ -14,10 +17,9 @@ async function dropTables() {
     console.log('Starting to drop tables...');
 
     await client.query(`
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
       DROP TABLE IF EXISTS posts;
-    `);
-
-    await client.query(`
       DROP TABLE IF EXISTS users;
     `);
 
@@ -53,6 +55,21 @@ async function createTables() {
         );
     `);
 
+    await client.query(`
+        CREATE TABLE tags (
+          id SERIAL PRIMARY KEY, 
+          name varchar(255) UNIQUE NOT NULL
+        );
+    `);
+
+    await client.query(`
+        CREATE TABLE post_tags (
+          "postId" INTEGER REFERENCES posts(id), 
+          "tagId" INTEGER REFERENCES tags(id),
+          UNIQUE ("postId", "tagId")
+        );
+    `);
+
     console.log('Finished building tables!');
   } catch (error) {
     console.error('Error building tables!');
@@ -80,24 +97,55 @@ async function createInitialPosts() {
   try {
     const [albert, sandra, glamgal] = await getAllUsers();
 
+    console.log("Starting to create posts...");
     await createPost({
       authorId: albert.id,
       title: "First Post", 
-      content: "This is my first post. I hope I love writing blogs as much as I love writing them." 
+      content: "This is my first post. I hope I love writing blogs as much as I love writing them.", 
+      tags: ["#happy", "#youcandoanything"]
     });
 
     await createPost({
       authorId: sandra.id,
       title: "Sandra Post #1", 
-      content: "Thanks @albert for inviting me to this app, I love it already" 
+      content: "Thanks @albert for inviting me to this app, I love it already",
+      tags: ["#happy", "#worst-day-ever"] 
     });
 
     await createPost({
       authorId: glamgal.id,
       title: "Posting Debut!", 
-      content: "Heyyyyy! Super excited to be making my first post! Stay tuned for more" 
+      content: "Heyyyyy! Super excited to be making my first post! Stay tuned for more",
+      tags: ["#happy", "#youcandoanything", "#canmandoeverything"] 
     });
+
+    console.log("Finished creating posts!");
   } catch (error) {
+    console.log("Error creating posts!")
+    throw error;
+  }
+}
+
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy', 
+      '#worst-day-ever', 
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
     throw error;
   }
 }
@@ -111,6 +159,7 @@ async function rebuildDB() {
     await createInitialUsers();
     await createInitialPosts();
   } catch (error) {
+    console.log("Error during rebuildDB");
     throw error;
   } 
 }
@@ -141,9 +190,19 @@ async function testDB() {
     });
     console.log(updatePostResult);
 
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"]
+    });
+    console.log("Result:", updatePostTagsResult);
+
     console.log("Calling getUserById with 1");
     const albert = await getUserById(1);
     console.log("Result:", albert);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log('Finished database tests!');
   } catch (error) {
